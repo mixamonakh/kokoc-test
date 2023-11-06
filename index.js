d28181ea61ea292639a8615b32d33a0d9c252ac0
Misha Monakhov <mixa.monakh@gmail.com>
2023-11-07 02:37:35 +0300

    прод версия

--- index.js
+++
@@ -1,7 +0,0 @@
-import './src/scss/stylesheet.scss';
-import './src/fonts/Roboto-Medium.ttf';
-import './src/fonts/Roboto-Regular.ttf';
-import './src/icons/like.svg';
-import './src/icons/like-active.svg';
-
-import './src/js/script'
